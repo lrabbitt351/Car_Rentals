379b6fe27e218d0562e7c4a822f71626a3850ec2
drkohlipk <drkohlipk@yahoo.com>
2016-12-15 16:02:02 -0800

    More initiliazation

--- Program.cs
+++ Program.cs
@@ -8,11 +8,11 @@ namespace carRentals
         public static void Main(string[] args)
         {
             var host = new WebHostBuilder()
+                .UseIISIntegration()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseStartup<Startup>()
                 .Build();
-
             host.Run();
         }
     }
